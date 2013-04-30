aeb56e00de6d93446d85beb557b464f986800949
David Estes <davydotcom@gmail.com>
2013-04-30 11:46:09 -0700

    modified some stuff

--- src/groovy/asset/pipeline/DirectiveProcessor.groovy
+++ src/groovy/asset/pipeline/DirectiveProcessor.groovy
@@ -42,15 +42,15 @@ class DirectiveProcessor {
     return buffer;
   }
 
-  def getDependencyTree(file) {
+	def getDependencyTree(file) {
     this.files << file
-    def tree = [file:file,tree:[]]
+		def tree = [file:file,tree:[]]
     if(file.class.name != 'java.io.File') {
       this.findDirectives(file,tree)
     }
 
     return tree
-  }
+	}
 
   def fileContents(file) {
 
@@ -62,30 +62,30 @@ class DirectiveProcessor {
 
   }
 
-  def findDirectives(fileSpec, tree) {
-    // try {
-      fileSpec.file.eachLine { line ->
+	def findDirectives(fileSpec, tree) {
+		// try {
+			fileSpec.file.eachLine { line ->
         if(!line) {
           return false;
           throw "End of Directive Set"
         }
         def directive = fileSpec.directiveForLine(line)
-        if(directive) {
-          directive = directive.trim().toLowerCase()
+				if(directive) {
+					directive = directive.trim().toLowerCase()
           def directiveArguments = directive.split(" ")
           def processor = DIRECTIVES[directiveArguments[0]]
 
           if(processor) {
             this."${processor}"(directiveArguments, fileSpec,tree)
           }
-        }
+				}
         return true
-      }
-    // } catch(except) {
+			}
+		// } catch(except) {
   //     //TODO: Narrow exception scope here please!
   //     log.info "Done Processing Directive for File"
-    // }
-  }
+		// }
+	}
 
   def requireSelfDirective(command, file, tree) {
     tree.tree << "self"
@@ -125,11 +125,9 @@ class DirectiveProcessor {
     if(file.class.name != 'java.io.File') {
       realFile = file.file
     }
-    println("Does file exist?: " + realFile.getAbsolutePath())
     def result = files.find { it ->
       return (it.class.name == 'java.io.File' && it.getAbsolutePath() == realFile.getAbsolutePath()) || it.file.getAbsolutePath() == realFile.getAbsolutePath()
     }
-    println("Result: ${result}")
     if(result) {
       return true
     } else {
@@ -138,19 +136,16 @@ class DirectiveProcessor {
   }
 
   def requireFileDirective(command, file, tree) {
-    println "Processing File Require For ${command}"
     def fileName = command[1]
     def newFile = null
     if(fileName.startsWith(File.separator)) {
       newFile = AssetHelper.fileForUri(fileName, this.contentType)
     } else {
       def relativeFileName = [relativePath(file.file),fileName].join(File.separator)
-      println "Seeking ${fileName}"
       newFile = AssetHelper.fileForUri(relativeFileName, this.contentType)
     }
 
     if(newFile) {
-      println "File Found"
       if(!isFileInTree(newFile,tree)) {
         tree.tree << getDependencyTree(newFile)
       }
