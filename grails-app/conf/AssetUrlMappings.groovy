09329b5cb724c429f4cffea82746ab62ddc64d8e
David Estes <davydotcom@gmail.com>
2013-05-02 06:32:45 -0700

    dev fix

--- grails-app/conf/AssetUrlMappings.groovy
+++ grails-app/conf/AssetUrlMappings.groovy
@@ -1,18 +1,9 @@
 class AssetUrlMappings {
 
 	static mappings = {
-
 		"/assets/$id**" (
 			controller: 'assets',
 			action: 'index'
 		)
-		"/$controller/$action?/$id?"{
-			constraints {
-				// apply constraints here
-			}
-		}
-
-		"/"(view:"/index")
-		"500"(view:'/error')
 	}
 }
