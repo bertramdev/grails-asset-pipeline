03b5bcf41830cbc5ed56afad7fc57a8663811c69
David Estes <davydotcom@gmail.com>
2013-05-07 09:53:52 -0400

    arent i silly

--- grails-app/conf/asset/pipeline/AssetsFilters.groovy
+++ grails-app/conf/asset/pipeline/AssetsFilters.groovy
@@ -5,8 +5,6 @@ class AssetsFilters {
     def filters = {
         all(controller:'assets', action:'*') {
             before = {
-                println "Getting The Forwarded Request URI"
-                println request.forwardURI
                 def file = ApplicationHolder.getApplication().getParentContext().getResource(request.forwardURI).getFile();
                 if(file.exists()) {
                     def format = servletContext.getMimeType(request.forwardURI)
