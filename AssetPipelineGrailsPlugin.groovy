b4c19c9d418414e2f41597f65f71a3efd7b3cf9a
Burt Beckwith <burt@burtbeckwith.com>
2013-07-29 11:08:07 -0400

    dropped the minimum Grails version to 2.0

--- AssetPipelineGrailsPlugin.groovy
+++ AssetPipelineGrailsPlugin.groovy
@@ -5,7 +5,7 @@ class AssetPipelineGrailsPlugin {
     // the plugin version
     def version = "0.1.0"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.2 > *"
+    def grailsVersion = "2.0 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"
