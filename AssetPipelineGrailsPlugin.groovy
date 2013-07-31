e55849f32ae0dcaea1aaab4b8cf42653dd682e5e
David Estes <davydotcom@gmail.com>
2013-07-31 09:10:06 -0400

    version bump

--- AssetPipelineGrailsPlugin.groovy
+++ AssetPipelineGrailsPlugin.groovy
@@ -5,7 +5,7 @@ import org.codehaus.groovy.grails.plugins.PluginManagerHolder
 import asset.pipeline.AssetFileArtefactHandler
 
 class AssetPipelineGrailsPlugin {
-    def version = "0.1.0"
+    def version = "0.1.1"
     def grailsVersion = "2.0 > *"
     def title = "Asset Pipeline Plugin"
     def author = "David Estes"
