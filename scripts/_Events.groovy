7df1b8301ba4610ee2160e808f26689d53c8fc30
David Estes <davydotcom@gmail.com>
2013-08-13 08:32:55 -0400

    adding warning message on war create if the assets have not been precompiled

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,9 +1,15 @@
 includeTargets << new File(assetPipelinePluginDir, "scripts/_AssetCompile.groovy")
 
 eventCreateWarStart = {warName, stagingDir ->
+
+  def assetDir = new File("web-app/assets")
+  // Warn of
+  if(!assetDir.exists()) {
+    event("StatusError",["It appears you have not precompiled your assets. Please do this before generating your WAR file!"])
+  }
 	// assetCompile()
 	// ant.exec(executable: "grails", dir: "${basedir}") {
- //        arg(value: "asset-precompile")
- //    }
+ 	//        arg(value: "asset-precompile")
+  //}
 
 }
