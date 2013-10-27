a3a8e43ff097847c75d556e5c2450041b31c0859
Bobby Warner <bobbywarner@gmail.com>
2013-10-27 15:01:10 -0500

    Initial Grails docs based on Wiki and plugin page

--- AssetPipelineGrailsPlugin.groovy
+++ AssetPipelineGrailsPlugin.groovy
@@ -6,7 +6,7 @@ class AssetPipelineGrailsPlugin {
 	def title           = "Asset Pipeline Plugin"
 	def author          = "David Estes"
 	def authorEmail     = "destes@bcap.com"
-	def description     = 'The Grails `asset-pipeline` is a plugin used for managing/processing static assets. These include processing, and minification of both css, and javascript files. It is also capable of being extended to compile custom static assets, such as coffeescript.'
+	def description     = 'The Asset-Pipeline is a plugin used for managing and processing static assets in Grails applications. Asset-Pipeline functions include processing and minification of both CSS and JavaScript files. It is also capable of being extended to compile custom static assets, such as CoffeeScript.'
 	def documentation   = "http://github.com/bertramdev/asset-pipeline"
 	def license         = "APACHE"
 	def organization    = [ name: "Bertram Capital", url: "http://www.bertramcapital.com/" ]
