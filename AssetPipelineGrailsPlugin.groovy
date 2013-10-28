946e3694b03098799d4d3df2bc799c9f20fc38f4
David Estes <davydotcom@gmail.com>
2013-10-28 09:56:18 -0400

    changed reference to documentation

--- AssetPipelineGrailsPlugin.groovy
+++ AssetPipelineGrailsPlugin.groovy
@@ -1,13 +1,13 @@
 import grails.util.Environment
 
 class AssetPipelineGrailsPlugin {
-	def version         = "1.0.1"
+	def version         = "1.0.2"
 	def grailsVersion   = "2.0 > *"
 	def title           = "Asset Pipeline Plugin"
 	def author          = "David Estes"
 	def authorEmail     = "destes@bcap.com"
 	def description     = 'The Asset-Pipeline is a plugin used for managing and processing static assets in Grails applications. Asset-Pipeline functions include processing and minification of both CSS and JavaScript files. It is also capable of being extended to compile custom static assets, such as CoffeeScript.'
-	def documentation   = "http://github.com/bertramdev/asset-pipeline"
+	def documentation   = "http://bertramdev.github.io/asset-pipeline"
 	def license         = "APACHE"
 	def organization    = [ name: "Bertram Capital", url: "http://www.bertramcapital.com/" ]
 	def issueManagement = [ system: "GITHUB", url: "http://github.com/bertramdev/asset-pipeline/issues" ]
