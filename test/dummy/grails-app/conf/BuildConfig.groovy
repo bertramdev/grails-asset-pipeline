6918dd0dd5fc8c4d9e1536dbcb3dc88a57273fd7
Ted Naleid <contact@naleid.com>
2014-07-10 20:05:57 -0500

    updating test application to grails 2.3.8

--- test/dummy/grails-app/conf/BuildConfig.groovy
+++ test/dummy/grails-app/conf/BuildConfig.groovy
@@ -1,16 +1,13 @@
-grails.servlet.version = "2.5" // Change depending on target container compliance (2.5 or 3.0)
+grails.servlet.version = "3.0" // Change depending on target container compliance (2.5 or 3.0)
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir = "target/work"
 grails.project.target.level = 1.6
 grails.project.source.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
-// uncomment (and adjust settings) to fork the JVM to isolate classpaths
-//grails.project.fork = [
-//   run: [maxMemory:1024, minMemory:64, debug:false, maxPerm:256]
-//]
-
+grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
@@ -26,13 +23,10 @@ grails.project.dependency.resolution = {
 
         grailsPlugins()
         grailsHome()
-        grailsCentral()
-
         mavenLocal()
+        grailsCentral()
         mavenCentral()
-
         // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
-        //mavenRepo "http://snapshots.repository.codehaus.org"
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
@@ -40,22 +34,20 @@ grails.project.dependency.resolution = {
 
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
-        runtime 'org.mozilla:rhino:1.7R4'
-        // runtime 'mysql:mysql-connector-java:5.1.22'
+        // runtime 'mysql:mysql-connector-java:5.1.27'
+        // runtime 'org.postgresql:postgresql:9.3-1100-jdbc41'
     }
 
     plugins {
-        runtime ":hibernate:$grailsVersion"
-        runtime ":jquery:1.8.3"
-        runtime ":resources:1.1.6"
+        build ":tomcat:7.0.52.1"
 
-        build ":tomcat:$grailsVersion"
+        // plugins for the compile step
+        compile ":scaffolding:2.0.3"
+        compile ':cache:1.1.2'
 
-        runtime ":database-migration:1.3.2"
-
-        compile ':cache:1.0.1'
+        // plugins needed at runtime but not for compilation
+        runtime ":hibernate:3.6.10.13" // or ":hibernate4:4.3.5.1"
     }
 }
 
 grails.plugin.location."asset-pipeline" = "../../"
-
