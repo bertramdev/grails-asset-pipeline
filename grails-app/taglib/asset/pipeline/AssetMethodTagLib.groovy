af8242ba0677ea543bb1376b662fff480f9acde8
Omasiri <Omasiri@novadge.com>
2014-09-25 00:32:58 +0100

    fixed minor error that occurs when attribute absolute=true inside asset tags

--- grails-app/taglib/asset/pipeline/AssetMethodTagLib.groovy
+++ grails-app/taglib/asset/pipeline/AssetMethodTagLib.groovy
@@ -4,53 +4,56 @@ import grails.util.Environment
 
 class AssetMethodTagLib {
 
-	static namespace = "g"
-	static returnObjectForTags = ['assetPath']
+    static namespace = "g"
+    static returnObjectForTags = ['assetPath']
 
-	def grailsApplication
-	def assetProcessorService
+    def grailsApplication
+    def assetProcessorService
+    def grailsLinkGenerator
 
-	def assetPath = { attrs ->
-		def src
+    def assetPath = { attrs ->
+        def src
         //unused
-		def ignorePrefix = false
+        def ignorePrefix = false
         def absolute = false
-		if (attrs instanceof Map) {
-			src = attrs.src
+        if (attrs instanceof Map) {
+            
+            src = attrs.src
             //unused
-			ignorePrefix = attrs.containsKey('ignorePrefix')? attrs.ignorePrefix : false
+            ignorePrefix = attrs.containsKey('ignorePrefix')? attrs.ignorePrefix : false
             absolute = attrs.containsKey('absolute') ? attrs.absolute : false
-		} else {
-			src = attrs
-		}
+        } else {
+            
+            src = attrs
+        }
+       
+        def conf = grailsApplication.config.grails.assets
 
-		def conf = grailsApplication.config.grails.assets
+        def assetUrl = assetUriRootPath(grailsApplication, request, absolute)
 
-		def assetUrl = assetUriRootPath(grailsApplication, request, absolute)
-
-		if(conf.precompiled) {
-			def realPath = conf.manifest.getProperty(src)
-			if(realPath) {
-				return "${assetUrl}${realPath}"
-			}
-		}
-		return "${assetUrl}${src}"
-	}
+        if(conf.precompiled) {
+            def realPath = conf.manifest.getProperty(src)
+            if(realPath) {
+                return "${assetUrl}${realPath}"
+            }
+        }
+        return "${assetUrl}${src}"
+    }
 
 
-	private assetUriRootPath(grailsApplication, request, absolute=false) {
-		def context = grailsApplication.mainContext //unused
-		def conf    = grailsApplication.config.grails.assets
-		def mapping = assetProcessorService.assetMapping
-		if(conf.url && conf.url instanceof Closure) {
-			return conf.url.call(request)
-		} else {
+    private assetUriRootPath(grailsApplication, request, absolute=false) {
+        def context = grailsApplication.mainContext //unused
+        def conf    = grailsApplication.config.grails.assets
+        def mapping = assetProcessorService.assetMapping
+        if(conf.url && conf.url instanceof Closure) {
+            return conf.url.call(request)
+        } else {
             if(absolute && !conf.url){
-                return grailsApplication.config.grails.serverURL + "${request.contextPath?.endsWith('/') ? '' : '/'}$mapping/"
+                return "${grailsLinkGenerator.getServerBaseURL()}" + "${request.contextPath?.endsWith('/') ? '' : '/'}$mapping/"
             }
             String relativePathToResource = (request.contextPath + "${request.contextPath?.endsWith('/') ? '' : '/'}$mapping/" )
             return conf.url ?: relativePathToResource
-		}
+        }
 
-	}
+    }
 }
