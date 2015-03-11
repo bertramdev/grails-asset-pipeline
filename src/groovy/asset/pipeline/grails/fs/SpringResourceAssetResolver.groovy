46c6888a05ae755ccd3784f8b5bf9b041cde54ed
David Estes <davydotcom@gmail.com>
2015-03-11 10:27:21 -0400

    removing debug printlns

--- src/groovy/asset/pipeline/grails/fs/SpringResourceAssetResolver.groovy
+++ src/groovy/asset/pipeline/grails/fs/SpringResourceAssetResolver.groovy
@@ -86,9 +86,7 @@ class SpringResourceAssetResolver extends AbstractAssetResolver<Resource> {
                 filePath = filePath.substring(i + prefixPath.size() + 1)
             }
             def result = cache[filePath]
-            println "Looking for ${filePath}"
             if(result) {
-                println "Found ${result}"
                 return result
             }
             return new EmptyResource()
@@ -198,7 +196,6 @@ class SpringResourceAssetResolver extends AbstractAssetResolver<Resource> {
             def relativePath = relativePathToResolver(res,prefixPath)
             results[relativePath] = res
         }
-        println "Cache ${results}"
         cache = results
     } 
 }
