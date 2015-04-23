8bea8d122ba7e4ad19ca8b207518863543dfee8e
Nolan Lancaster <nlancaster@triu.com>
2015-04-23 15:33:45 -0400

    Pass enableSourceMaps config setting to AssetCompiler

--- scripts/_AssetCompile.groovy
+++ scripts/_AssetCompile.groovy
@@ -32,6 +32,7 @@ target(assetCompile: "Precompiles assets in the application as specified by the 
 	assetConfig.minifyOptions = config.grails.assets.minifyOptions
 	assetConfig.compileDir = "${basedir}/target/assets"
 	assetConfig.excludesGzip = config.grails.assets.excludesGzip
+    assetConfig.enableSourceMaps = config.grails.assets.enableSourceMaps
 
 	//Add Resolvers for Grails
 	assetPipelineConfigHolder.registerResolver(fileSystemAssetResolver.newInstance('application','grails-app/assets'))
