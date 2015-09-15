25f891debedae18ad4765075711a72a8b45999ef
rgoldberg <ross.goldberg@gmail.com>
2015-09-15 06:51:40 -0400

    replaced AssetMethodTagLib#assetUriRootPath(boolean) with assetProcessorService.assetBaseUrl(...)

--- grails-app/taglib/asset/pipeline/grails/AssetMethodTagLib.groovy
+++ grails-app/taglib/asset/pipeline/grails/AssetMethodTagLib.groovy
@@ -1,9 +1,6 @@
 package asset.pipeline.grails
 
 
-import static org.apache.commons.lang.StringUtils.trimToEmpty
-
-
 class AssetMethodTagLib {
 
 	static namespace = 'g'
@@ -11,8 +8,6 @@ class AssetMethodTagLib {
 
 
 	def assetProcessorService
-	def grailsApplication
-	def grailsLinkGenerator
 
 
 	def assetPath = {final def attrs ->
@@ -30,27 +25,6 @@ class AssetMethodTagLib {
 			absolute = false
 		}
 
-		return assetUriRootPath(absolute) + assetProcessorService.getAssetPath(src)
-	}
-
-	private String assetUriRootPath(final boolean absolute) {
-		final String mapping = assetProcessorService.assetMapping
-
-		def configUrl = grailsApplication.config.grails.assets.url
-
-		if (configUrl instanceof Closure) {
-			configUrl = configUrl.call(request)
-		}
-
-		if (configUrl) {
-			return configUrl
-		}
-		else if (absolute) {
-			return [grailsLinkGenerator.serverBaseURL, "$mapping/"].join('/')
-		}
-
-		final String contextPath = trimToEmpty(grailsLinkGenerator.contextPath)
-
-		return contextPath + "${contextPath.endsWith('/') ? '' : '/'}$mapping/"
+		return assetProcessorService.assetBaseUrl(request, absolute) + assetProcessorService.getAssetPath(src)
 	}
 }
