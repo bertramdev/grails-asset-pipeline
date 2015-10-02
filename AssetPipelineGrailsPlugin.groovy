e0d8b1fc06b9e0b19047f703d7adc43b94e6f895
David Estes <davydotcom@gmail.com>
2015-10-02 18:03:02 -0400

    updating travis build

--- AssetPipelineGrailsPlugin.groovy
+++ AssetPipelineGrailsPlugin.groovy
@@ -26,7 +26,7 @@ import asset.pipeline.grails.LinkGenerator
 import asset.pipeline.grails.fs.SpringResourceAssetResolver
 
 class AssetPipelineGrailsPlugin {
-    def version         = "2.5.2"
+    def version         = "2.5.4"
     def grailsVersion   = "2.2 > *"
     def title           = "Asset Pipeline Plugin"
     def author          = "David Estes"
