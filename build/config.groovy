246e721b7ca2d37ec8348ea96c53a99710e0e089
David Estes <davydotcom@gmail.com>
2015-10-03 12:53:53 -0400

    whoops

--- build/config.groovy
+++
@@ -1,8 +0,0 @@
-
-withConfig(configuration) {
-    inline(phase: 'CONVERSION') { source, context, classNode ->
-        classNode.putNodeMetaData('projectVersion', '3.0.7')
-        classNode.putNodeMetaData('projectName', 'asset-pipeline')
-        classNode.putNodeMetaData('isPlugin', 'true')
-    }
-}
--- build/resources/test/META-INF/assets/javascripts/test.js
+++
@@ -1,4 +0,0 @@
-//=require_self
-//=require_tree libs
-
-console.log("Initial Testing Stack for dependency load order");
